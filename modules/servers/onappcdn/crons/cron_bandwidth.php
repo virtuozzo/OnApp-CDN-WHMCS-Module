b370953f061d9b0ab155f9b2b5e309d7a98ee3be
Yuriy Yakubskiy <yuriy.yakubskiy@onapp.com>
2012-03-07 17:10:05 +0200

    To the previous commit

--- modules/servers/onappcdn/crons/cron_bandwidth.php
+++ modules/servers/onappcdn/crons/cron_bandwidth.php
@@ -219,20 +219,5 @@ function runSQL() {
 	}
 }
 
-function redirect($url) {
-        if (!headers_sent()) {
-        header('Location: ' . $url);
-        exit;
-    } else {
-        echo '<script type="text/javascript">';
-        echo 'window.location.href="' . $url . '";';
-        echo '</script>';
-        echo '<noscript>';
-        echo '<meta http-equiv="refresh" content="0;url=' . $url . '" />';
-        echo '</noscript>';
-        exit;
-    };
-}
-
 echo 'CDN Billing CronJob was Finished Successfully', PHP_EOL;
 
