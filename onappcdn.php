b61059d0ccde2f4c285dd476f101927f63c8bfb8
Yuriy Yakubskiy <yuriy.yakubskiy@onapp.com>
2012-03-28 10:33:36 +0300

    Switch off error reporting

--- onappcdn.php
+++ onappcdn.php
@@ -1,7 +1,7 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-error_reporting( E_ALL );
-ini_set( 'display_errors', 1 );
+//error_reporting( E_ALL );
+//ini_set( 'display_errors', 1 );
 
 define("CLIENTAREA",true);
 
