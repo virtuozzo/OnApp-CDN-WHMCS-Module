6a0bf4ec8ba2b329825e75c569a6a2b0ed5ee703
Lev Bartashevsky <devman@iMac.local>
2013-04-16 13:44:10 +0300

    reformat code

--- includes/hooks/onappcdn.php
+++ includes/hooks/onappcdn.php
@@ -1,78 +1,79 @@
 <?php
-
 /**
  * Adds payment to OnApp CP after invoice is marked as paid in WHMCS
  *
  * @param array $vars invoice ID
+ *
  * @return void
  */
 function onappcdn_invoice_paid( $vars ) {
-    
-    if ( ! defined('ONAPP_WRAPPER_INIT') )
-        define('ONAPP_WRAPPER_INIT', dirname(__FILE__).
-               '/../wrapper/OnAppInit.php');
+	if( ! defined( 'ONAPP_WRAPPER_INIT' ) ) {
+		define( 'ONAPP_WRAPPER_INIT', dirname( __FILE__ ) .
+				'/../wrapper/OnAppInit.php' );
+	}
 
-    if ( file_exists( ONAPP_WRAPPER_INIT ) )
-        require_once ONAPP_WRAPPER_INIT;
-    
+	if( file_exists( ONAPP_WRAPPER_INIT ) ) {
+		require_once ONAPP_WRAPPER_INIT;
+	}
+
 	$invoice_id = $vars[ 'invoiceid' ];
 
-    $query = "
-        SELECT
-            i.subtotal as amount,
-            onappc.onapp_user_id,
-            s.ipaddress,
-            s.username,
-            s.password,
-            s.hostname,
-            c.currency,
-            curr.rate,
-            h.id       as hostingid
-        FROM
-            tblinvoices as i
-        LEFT JOIN
-            tblhosting  as h
-            ON i.notes = h.id
-        LEFT JOIN
-            tblservers as s
-            ON s.id = h.server
-        LEFT JOIN
-            tblonappcdnclients as onappc
-            ON h.id = onappc.service_id
-        LEFT JOIN
-            tblclients as c
-            ON c.id = h.userid
-        LEFT JOIN
-            tblcurrencies as curr
-            ON curr.id = c.currency
-        WHERE
-            i.id = $invoice_id
-    ";
+	$query = 'SELECT
+				i.subtotal AS amount,
+				onappc.onapp_user_id,
+				s.ipaddress,
+				s.username,
+				s.password,
+				s.hostname,
+				c.currency,
+				curr.rate,
+				h.id AS hostingid
+			FROM
+				tblinvoices AS i
+			LEFT JOIN
+				tblhosting  AS h
+				ON i.notes = h.id
+			LEFT JOIN
+				tblservers AS s
+				ON s.id = h.server
+			LEFT JOIN
+				tblonappcdnclients AS onappc
+				ON h.id = onappc.service_id
+			LEFT JOIN
+				tblclients AS c
+				ON c.id = h.userid
+			LEFT JOIN
+				tblcurrencies AS curr
+				ON curr.id = c.currency
+			WHERE
+				i.id = $invoice_id';
 
-    $result = full_query( $query );
+	$result = full_query( $query );
 
-    if ( ! $result )
-        return;
+	if( ! $result ) {
+		return;
+	}
 
-    $row    = mysql_fetch_assoc( $result );
+	$row = mysql_fetch_assoc( $result );
 
 // if main product invoice then don't add payment.
-    if ( ! $row['hostingid'] )
-        return;
+	if( ! $row[ 'hostingid' ] ) {
+		return;
+	}
 
-    if ( ! $onapp ) {
-        $onapp = new OnApp_Factory(
-            ( $row['hostname'] ) ? $row['hostname'] : $row['ipaddress'],
-            $row['username'],
-            decrypt( $row['password'])
-        );
-    }
+	if( ! isset( $onapp ) ) {
+		$onapp = new OnApp_Factory(
+			( $row[ 'hostname' ] ) ? $row[ 'hostname' ] : $row[ 'ipaddress' ],
+			$row[ 'username' ],
+			decrypt( $row[ 'password' ] )
+		);
+	}
 
-    $payment = $onapp->factory('Payment', true );
-    $payment->_user_id        = $row['onapp_user_id'];
-    $payment->_amount         = round( $row['amount'] / $row['rate'], 2 );
-    $payment->_invoice_number = $invoice_id;
-    $payment->save();
+	$payment = $onapp->factory( 'Payment', true );
+	$payment->_user_id = $row[ 'onapp_user_id' ];
+	$payment->_amount = round( $row[ 'amount' ] / $row[ 'rate' ], 2 );
+	$payment->_invoice_number = $invoice_id;
+	$payment->save();
 }
 
 add_hook( 'InvoicePaid', 1, 'onappcdn_invoice_paid' );
