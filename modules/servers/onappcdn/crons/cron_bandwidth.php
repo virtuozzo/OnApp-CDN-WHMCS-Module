92de785739dfc6a49bfa65b2975399edb7d057bb
Lev Bartashevsky <devman@iMac.local>
2013-05-31 16:32:50 +0300

    implement cron testing

--- modules/servers/onappcdn/crons/cron_bandwidth.php
+++ modules/servers/onappcdn/crons/cron_bandwidth.php
@@ -1,206 +1,208 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-ini_set('html_errors', 1);
-@date_default_timezone_set('UTC');
+error_reporting( E_ALL );
+ini_set( "display_errors", 1 );
+ini_set( 'html_errors', 1 );
+@date_default_timezone_set( 'UTC' );
 @ini_set( 'memory_limit', '512M' );
 @ini_set( 'max_execution_time', 0 );
 @set_time_limit( 0 );
 
-define("ROOT", realpath( dirname(__FILE__) .'/../../../../' ). '/' );
+define( 'ROOT', dirname( dirname( dirname( dirname( dirname( $_SERVER[ 'SCRIPT_FILENAME' ] ) ) ) ) ) .  '/' );
 
-require_once ROOT . "dbconnect.php";
-require_once ROOT . "includes/functions.php";
-require_once ROOT . "includes/clientareafunctions.php";
-require_once ROOT . "includes/wrapper/OnAppInit.php";
+require_once ROOT . 'dbconnect.php';
+require_once ROOT . 'includes/functions.php';
+require_once ROOT . 'includes/clientareafunctions.php';
+require_once ROOT . 'includes/wrapper/OnAppInit.php';
 
-if ( $argv[1] == '--fullupdate'  ) {
-    new Cron_Job( true );
-    
-} else {
-    new Cron_Job();
+if( $argv[ 1 ] == '--fullupdate' ) {
+	new Cron_Job( true );
+}
+else {
+	new Cron_Job();
 }
 
 class Cron_Job {
-    private $first_time = false;
-    private $sql_file;
-    private $cron_query = "
+	private $first_time = false;
+	private $sql_file;
+	private $cron_query = "
         SELECT
             h.userid,
             h.domain,
-            c.email               as whmcsclientemail,
+            c.email               AS whmcsclientemail,
             s.hostname,
             s.ipaddress,
-            s.id                  as serverid,
+            s.id                  AS serverid,
             onappc.onapp_user_id,
-            onappc.email          as username,
+            onappc.email          AS username,
             onappc.password,
-            h.id                  as hostingid,
-            curr.rate             as currency_rate,
-            p.configoption6       as price
+            h.id                  AS hostingid,
+            curr.rate             AS currency_rate,
+            p.configoption6       AS price
         FROM
-            tblservers as s
+            tblservers AS s
         LEFT JOIN
-            tblhosting as h
+            tblhosting AS h
             ON h.server = s.id
         LEFT JOIN
-            tblonappcdnclients as onappc
+            tblonappcdnclients AS onappc
             ON onappc.service_id = h.id
         LEFT JOIN
-            tblproducts as p
+            tblproducts AS p
             ON h.packageid = p.id
         LEFT JOIN
-            tblclients as c
+            tblclients AS c
             ON h.userid = c.id
         LEFT JOIN
-            tblcurrencies as curr
+            tblcurrencies AS curr
             ON curr.id = c.currency
         WHERE
             s.type = 'onappcdn' AND
             onappc.onapp_user_id != ''
-    "; 
-    
-    private $max_stat_time_query = "SELECT MAX(stat_time) as stat_time FROM `tblonappcdn_billing`";
-    
-    /**
-     * Class construct
-     * 
-     * @param boolean $full_update  
-     */
-    public function __construct( $full_update = false ) {
-        
-        $this->first_time = $full_update;
-        $this->sql_file = dirname( __FILE__ ) . '/billing.sql';
-        $this->bootstrap();
-        $this->run_cron();
-    }
-    
-    /**
-     * Cron bootstrap
-     * 
-     * @return void
-     */
-    private function bootstrap(){
-        $bandwidth_table_exists = ( boolean )mysql_num_rows ( full_query("SHOW TABLES LIKE 'tblonappcdn_bandwidth'") );
-        $billing_table_exists   = ( boolean )mysql_num_rows ( full_query("SHOW TABLES LIKE 'tblonappcdn_billing'") );
+    ";
+	private $max_stat_time_query = "SELECT MAX(stat_time) AS stat_time FROM `tblonappcdn_billing`";
 
-        if ( $bandwidth_table_exists || ! $billing_table_exists ) {
+	/**
+	 * Class construct
+	 *
+	 * @param boolean $full_update
+	 */
+	public function __construct( $full_update = false ) {
+		$this->first_time = $full_update;
+		$this->sql_file = dirname( __FILE__ ) . '/billing.sql';
+		$this->bootstrap();
+		$this->run_cron();
+	}
 
-            full_query('DROP TABLE IF EXISTS tblonappcdn_bandwidth');
+	/**
+	 * Cron bootstrap
+	 *
+	 * @return void
+	 */
+	private function bootstrap() {
+		$bandwidth_table_exists = ( boolean )mysql_num_rows( full_query( "SHOW TABLES LIKE 'tblonappcdn_bandwidth'" ) );
+		$billing_table_exists = ( boolean )mysql_num_rows( full_query( "SHOW TABLES LIKE 'tblonappcdn_billing'" ) );
 
-            if( file_exists( $this->sql_file ) ) {
-                $this->runSQLFromFile( $this->sql_file );
-            } else {
-                $this->error( 'Module Install Error:File not found ' . $this->sql_file );
-            }
+		if( $bandwidth_table_exists || ! $billing_table_exists ) {
 
-            $this->first_time = true;
-        }        
-    }
-    
-    /**
-     * Run sql form the file
-     * 
-     * @param string $path 
-     */
-    private function runSQLFromFile( $path ) {
-        $sql  = file_get_contents( $path );
-        $sql  = explode( PHP_EOL . PHP_EOL, $sql );
+			full_query( 'DROP TABLE IF EXISTS tblonappcdn_bandwidth' );
 
-        foreach( $sql as $qry ) {
-            full_query( $qry );
-        }
-    }    
+			if( file_exists( $this->sql_file ) ) {
+				$this->runSQLFromFile( $this->sql_file );
+			}
+			else {
+				$this->error( 'Module Install Error:File not found ' . $this->sql_file );
+			}
 
-    /**
-     * Run cron
-     * 
-     * @return void 
-     */
-    private function run_cron(){
-        $today = date('Y-m-d');
-        $tomorrow = date('Y-m-d', strtotime( $today ) + 86400 );
-        
-        $result = full_query( $this->cron_query );
-        
-        if ( ! $result ){
-            $this->error('Main query error: ' . mysql_error() );
-        }
-        
-        if ( mysql_num_rows($result) < 1 ) {
-            $this->error( 'No active CDN resources exiting..' );
-        }
-        
-        while ( $row = mysql_fetch_assoc( $result ) ) {
-            
-            $this->debug('****************************************************** H O S T I N G   L I N E ********************************************'); 
-            
-            $onapp = new OnApp_Factory(
-                ( $row['hostname'] ) ? $row['hostname'] : $row['ipaddress'],
-                $row['username'],
-                $row['password']
-            ); 
-            
-            if ( $onapp->getErrorsAsArray() ) {
-                $this->debug( 'OnApp Login Error' . PHP_EOL . $onapp->getErrorsAsString( ) );
-                continue;
-            }            
-            
-            $_resource  = $onapp->factory('CDNResource', true );
-            $_resources = $_resource->getList( );
-            
-            $resources = array();
-            foreach( $_resources as $resource ){
-                if( $resource->_user_id == $row['onapp_user_id'] ){
-                    $resources[] = $resource;
-                }
-            }
+			$this->first_time = true;
+		}
+	}
 
-            if (  $_resource->getErrorsAsArray() ) {
-                $this->debug( 'Error Loading OnApp_CDNResource Object '  . PHP_EOL . $_resource->getErrorsAsString() );
-                continue;
-            }
+	/**
+	 * Run sql form the file
+	 *
+	 * @param string $path
+	 */
+	private function runSQLFromFile( $path ) {
+		$sql = file_get_contents( $path );
+		$sql = explode( PHP_EOL . PHP_EOL, $sql );
 
-            if ( count( $resources ) < 1 ) {
-                $this->debug('This user have no CDN Resources. Skipping' . PHP_EOL );
-                continue;
-            }
-            
-            foreach ( $resources as $resource ) {
-                
-                $bs = $onapp->factory('CDNResource_BillingStatistic', true );
+		foreach( $sql as $qry ) {
+			full_query( $qry );
+		}
+	}
 
-                $this->debug( $this->max_stat_time_query );
-                $max_result = full_query( $this->max_stat_time_query );
-                
-                if ( ! $max_result ) {
-                    $this->debug( 'Max Result Query Error: ' . mysql_error() );
-                    continue;
-                } elseif ( mysql_num_rows($max_result) < 1 ){
-                    $this->debug('Billing Table is Empty, launch fullupdate');
-                    $this->first_time = true;
-                } else { 
-                    $max_row = mysql_fetch_assoc( $max_result );
-                    $this->debug( print_r( $max_row['stat_time'] ) );
-                }               
-                
-                if ( $this->first_time ){
-                    $_bs = $bs->getList( $resource->_id, array( 'per_page' => 'all' ) );
-                } else {
-                   
-                    $_bs = $bs->getList( 
-                            $resource->_id, 
-                            array( 
-                                'per_page' => 'all',
-                                'period[startdate]' => date('Y-m-d H:i:s', strtotime( $max_row['stat_time'] ) - 7200 ),
-                                'period[enddate]'   => $tomorrow,
-                            ) );
-                }
-                
-                $this->debug( print_r($bs->getResponse()) );
+	/**
+	 * Run cron
+	 *
+	 * @return void
+	 */
+	private function run_cron() {
+		$today = date( 'Y-m-d' );
+		$tomorrow = date( 'Y-m-d', strtotime( $today ) + 86400 );
 
-                foreach ( $_bs as $_obj ) {
-                    $replace_query = "
+		$result = full_query( $this->cron_query );
+
+		if( ! $result ) {
+			$this->error( 'Main query error: ' . mysql_error() );
+		}
+
+		if( mysql_num_rows( $result ) < 1 ) {
+			$this->error( 'No active CDN resources exiting..' );
+		}
+
+		while( $row = mysql_fetch_assoc( $result ) ) {
+			$this->debug( '****************************************************** H O S T I N G   L I N E ********************************************' );
+			//print_r( $row );continue;
+
+			$onapp = new OnApp_Factory(
+				( $row[ 'hostname' ] ) ? $row[ 'hostname' ] : $row[ 'ipaddress' ],
+				$row[ 'username' ],
+				$row[ 'password' ]
+			);
+
+			if( $onapp->getErrorsAsArray() ) {
+				$this->debug( 'OnApp Login Error' . PHP_EOL . $onapp->getErrorsAsString() );
+				continue;
+			}
+
+			$_resource = $onapp->factory( 'CDNResource', true );
+			$_resources = $_resource->getList();
+
+			$resources = array();
+			foreach( $_resources as $resource ) {
+				if( $resource->_user_id == $row[ 'onapp_user_id' ] ) {
+					$resources[ ] = $resource;
+				}
+			}
+
+			if( $_resource->getErrorsAsArray() ) {
+				$this->debug( 'Error Loading OnApp_CDNResource Object ' . PHP_EOL . $_resource->getErrorsAsString() );
+				continue;
+			}
+
+			if( count( $resources ) < 1 ) {
+				$this->debug( 'This user have no CDN Resources. Skipping' . PHP_EOL );
+				continue;
+			}
+
+			foreach( $resources as $resource ) {
+
+				$bs = $onapp->factory( 'CDNResource_BillingStatistic', true );
+
+				$this->debug( $this->max_stat_time_query );
+				$max_result = full_query( $this->max_stat_time_query );
+
+				if( ! $max_result ) {
+					$this->debug( 'Max Result Query Error: ' . mysql_error() );
+					continue;
+				}
+				elseif( mysql_num_rows( $max_result ) < 1 ) {
+					$this->debug( 'Billing Table is Empty, launch fullupdate' );
+					$this->first_time = true;
+				}
+				else {
+					$max_row = mysql_fetch_assoc( $max_result );
+					$this->debug( print_r( $max_row[ 'stat_time' ] ) );
+				}
+
+				if( $this->first_time ) {
+					$_bs = $bs->getList( $resource->_id, array( 'per_page' => 'all' ) );
+				}
+				else {
+
+					$_bs = $bs->getList(
+						$resource->_id,
+						array(
+							 'per_page'          => 'all',
+							 'period[startdate]' => date( 'Y-m-d H:i:s', strtotime( $max_row[ 'stat_time' ] ) - 7200 ),
+							 'period[enddate]'   => $tomorrow,
+						) );
+				}
+
+				$this->debug( print_r( $bs->getResponse() ) );
+
+				foreach( $_bs as $_obj ) {
+					$replace_query = "
                         REPLACE INTO
                             tblonappcdn_billing(
                                 cost,
@@ -223,36 +225,35 @@ class Cron_Job {
                                 $row[currency_rate]
                             )
                     ";
-                    
-                    $this->debug( $replace_query );
-                    
-                    $replace_result = full_query( $replace_query );
-                    
-                    if ( ! $replace_result ) {
-                        $this->debug( 'Replace error: ' . mysql_error());
-                    }
-                }                 
-            }
-        }
-    }
 
-    /**
-     * Error message and exit 
-     * 
-     * @param string $message 
-     */
-    private function error( $message ) {
-        echo PHP_EOL . $message . PHP_EOL ;
-        exit();
-    }
-    
-    /**
-     * Debug message
-     * 
-     * @param string $message 
-     */
-    private function debug( $message ){
-        echo PHP_EOL . $message . PHP_EOL . PHP_EOL;
-    }
-    
+					$this->debug( $replace_query );
+
+					$replace_result = full_query( $replace_query );
+
+					if( ! $replace_result ) {
+						$this->debug( 'Replace error: ' . mysql_error() );
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Error message and exit
+	 *
+	 * @param string $message
+	 */
+	private function error( $message ) {
+		echo PHP_EOL . $message . PHP_EOL;
+		exit();
+	}
+
+	/**
+	 * Debug message
+	 *
+	 * @param string $message
+	 */
+	private function debug( $message ) {
+		echo PHP_EOL . $message . PHP_EOL . PHP_EOL;
+	}
 }
